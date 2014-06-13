af088910c88e7a2b9bfb6c916683114f0883fcb6
Nitin <redknitin@gmail.com>
2014-06-13 22:22:10 +0400

    Output to files with default names

--- srcgen.py
+++ srcgen.py
@@ -3,16 +3,53 @@ import os
 def make_template_file(lstclassnames):
 	pass
 
+def make_forms_file(lstclassnames):
+	retval = '''\
+from django.forms import ModelForm
+from {0}.models import {1}
+
+class BaseForm(ModelForm):
+	required_css_class='required'
+	class Meta:
+		abstract=True
+
+'''.format(
+		os.path.dirname(os.path.realpath(__file__)).split(os.sep)[-1], #should we use os.getcwd instead of dirname of realpath?
+		', '.join(lstclassnames)
+	)
+	for iter in lstclassnames:
+		retval = retval + '''\
+class {0}Form(BaseForm):
+	class Meta:
+		model={0}
+
+'''.format(iter)
+	return retval
+
 def make_views_file(lstclassnames):
-	retval = ''
+	retval = '''
+from django.http import HttpResponse, HttpResponseRedirect
+from {0}.forms import {2}
+from {0}.models import {1}
+from django.shortcuts import render
+from django.core.urlresolvers import reverse
+
+def home(request):
+	return HttpResponse('Home page under construction', mimetype='text/plain')
+
+'''.format(
+		os.path.dirname(os.path.realpath(__file__)).split(os.sep)[-1],
+		', '.join(lstclassnames),
+		'Form, '.join(lstclassnames)
+	)
 	for iter in lstclassnames:
 		retval = retval + '''\
 def {0}_edit(request):
-	form=forms.TransactionForm()
+	form=forms.{1}Form()
 	return render(request, 'edit_{0}.html', locals())
 
 def {0}_save(request):
-	form=forms.TransactionForm(request.POST)
+	form=forms.{1}Form(request.POST)
 	if form.is_valid():
 		form.save()
 		return HttpResponseRedirect(reverse('list_{0}'))
@@ -26,16 +63,28 @@ def {0}_remove(request):
 	pass
 	\
 '''.format(iter.lower(), iter)
-	print(retval)
+	return retval
 
 def make_urls_file(lstclassnames):
 	retval = ''
+	retval = retval + 'from {0} import views'.format(
+		os.path.dirname(os.path.realpath(__file__)).split(os.sep)[-1]
+	) + os.linesep
+	retval = retval + '''\
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+'''
 	for iter in lstclassnames:
 		retval = retval + 'url(r\'^{0}/edit$\', views.{0}_edit),'.format(iter.lower()) + os.linesep
 		retval = retval + 'url(r\'^{0}/index$\', views.{0}_list),'.format(iter.lower()) + os.linesep
 		retval = retval + 'url(r\'^{0}/delete$\', views.{0}_remove),'.format(iter.lower()) + os.linesep
 		retval = retval + os.linesep
-	print(retval)
+	retval = retval + ')'
+	return retval
 
 def make_admin_file(lstclassnames):
 	retval = '''\
@@ -50,7 +99,7 @@ for iterclass in classes:
 		os.path.dirname(os.path.realpath(__file__)).split(os.sep)[-1], #should we use os.getcwd instead of dirname of realpath?
 		', '.join(lstclassnames)
 	)
-	print(retval)
+	return retval
 
 import re
 
@@ -60,18 +109,32 @@ import re
 #match looks only at the beginning of the string
 #search is a proper search
 
-cre = re.compile(r'class +(.*) *'+re.escape('('))
-modelfile = open('models.py', 'r')
-lstclassnames = []
-for line in modelfile:
-	cap = cre.match(line)
-	if not cap is None:
-		classname = cap.group(1)
-		if not classname.endswith('Base'): #ignore base classes
-			lstclassnames.append(classname)
-if len(lstclassnames)>0:
-	#make_urls_file(lstclassnames)
-	make_views_file(lstclassnames)
-	#make_admin_file(lstclassnames)
-	pass
-modelfile.close()
+def writetofile(filename, content):
+	f=open(filename, 'w')
+	f.write(content)
+	f.close()
+
+def generate_code(modelsfile='models.py', viewsfile=None, urlsfile=None, adminfile=None, formsfile=None):
+	cre = re.compile(r'class +(.*) *'+re.escape('('))
+	modelsfile = open(modelsfile, 'r')
+	lstclassnames = []
+	for line in modelsfile:
+		cap = cre.match(line)
+		if not cap is None:
+			classname = cap.group(1)
+			if not classname.endswith('Base'): #ignore base classes
+				lstclassnames.append(classname)
+	if len(lstclassnames)>0:
+		if not urlsfile is None:
+			writetofile(urlsfile, make_urls_file(lstclassnames))
+		if not viewsfile is None:
+			writetofile(viewsfile, make_views_file(lstclassnames))
+		if not adminfile is None:
+			writetofile(adminfile, make_admin_file(lstclassnames))
+		if not formsfile is None:
+			writetofile(formsfile, make_forms_file(lstclassnames))
+		pass
+	modelsfile.close()
+
+if __name__=='__main__':
+	generate_code('models.py', 'views.py', 'urls.py', 'admin.py')
